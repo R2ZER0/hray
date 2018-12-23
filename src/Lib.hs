5c97f28a2be04995db5663701c75bfacfda31e9b
Rikki Guy <git@rikkiguy.me.uk>
2018-12-23 16:57:38 +0000

    Solid colour

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,20 +5,33 @@ module Lib
 import Data.Vec3
 import Text.Pretty.Simple (pPrint, pShow)
 
-lowerLeft = CVec3 (-0.5) (-0.5) 1.0
-xUnit xs = CVec3 (1.0/(fromIntegral xs)) 0 0
-yUnit ys = CVec3 0 (1.0/(fromIntegral ys)) 0
+type Vec = CVec3
+vec x y z = CVec3 x y z
 
-rayAt xs ys x y = lowerLeft <+> ((xUnit xs) .^ (fromIntegral x)) <+> ((yUnit ys) .^ (fromIntegral y))
+-- Ray origin direction
+data Ray = Ray Vec Vec deriving (Show, Eq)
 
-raysAt :: Int -> Int -> Int -> Int -> [(Int, Int, CVec3)]
-raysAt xs ys x y
+lowerLeft = vec (-0.5) (-0.5) 1.0
+xUnit xs = vec (1.0/(fromIntegral xs)) 0 0
+yUnit ys = vec 0 (1.0/(fromIntegral ys)) 0
+
+cameraLocation = vec 0 0 1
+
+rayAt xs ys x y = Ray cameraLocation (lowerLeft <+> ((xUnit xs) .^ (fromIntegral x)) <+> ((yUnit ys) .^ (fromIntegral y)))
+
+viewPixelsAt xs ys x y
     | x >= xs && y >= ys = []
-    | x >= xs && y < ys = raysAt xs ys 0 (y+1)
-    | otherwise =  (x, y, rayAt xs ys x y) : raysAt xs ys (x+1) y
+    | x >= xs && y < ys = viewPixelsAt xs ys 0 (y+1)
+    | otherwise = (x, y) : viewPixelsAt xs ys (x+1) y
 
-viewRays :: Int -> Int -> [(Int, Int, CVec3)]
-viewRays xs ys = raysAt xs ys 0 0
+viewPixels xs ys = viewPixelsAt xs ys 0 0
+
+viewRays xs ys = fmap (\(x, y) -> (x, y, rayAt xs ys x y)) $ viewPixels xs ys 
+
+
+trace (Ray origin dir) = vec 1.0 0.0 0.0
+
+view xs ys = fmap (\(x, y, ray) -> trace ray) $ viewRays xs ys
 
 someFunc :: IO ()
-someFunc = pPrint $ viewRays (4 :: Int) (4 :: Int)
+someFunc = pPrint $ view (2 :: Int) (2 :: Int)
