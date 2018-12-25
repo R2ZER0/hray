cce510a420c0c9f22cbfcf951984b2caa9131af9
Rikki Guy <git@rikkiguy.me.uk>
2018-12-25 18:34:11 +0000

    It doesn't crash the window manager anymore!

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( someFunc
     ) where
 
+import Control.Monad
 import Data.Vec3
 import Text.Pretty.Simple (pPrint, pShow)
+import qualified SDL as SDL
+import qualified Control.Exception as Exception
 
 type Vec = CVec3
 vec x y z = CVec3 x y z
@@ -26,7 +30,7 @@ viewPixelsAt xs ys x y
 
 viewPixels xs ys = viewPixelsAt xs ys 0 0
 
-viewRays xs ys = fmap (\(x, y) -> (x, y, rayAt xs ys x y)) $ viewPixels xs ys 
+viewRays xs ys = fmap (\(x, y) -> (x, y, rayAt xs ys x y)) $ viewPixels xs ys
 
 
 trace (Ray origin dir) = vec 1.0 0.0 0.0
@@ -34,4 +38,35 @@ trace (Ray origin dir) = vec 1.0 0.0 0.0
 view xs ys = fmap (\(x, y, ray) -> trace ray) $ viewRays xs ys
 
 someFunc :: IO ()
-someFunc = pPrint $ view (2 :: Int) (2 :: Int)
+someFunc = do
+    let picture = view (2 :: Int) (2 :: Int)
+    putStrLn $ show picture
+    renderPicture picture
+
+-- Utility functions
+eventIsPressQ ev = case SDL.eventPayload ev of
+    SDL.KeyboardEvent keyboardEvent ->
+        SDL.keyboardEventKeyMotion keyboardEvent == SDL.Pressed && SDL.keysymKeycode (SDL.keyboardEventKeysym keyboardEvent) == SDL.KeycodeQ
+    _ -> False
+
+renderPicture img = do
+    window <- SDL.createWindow "App" SDL.defaultWindow
+    Exception.catch (runApp window) (catchErr window)
+    where
+        runApp window = do
+            renderer <- SDL.createRenderer window (-1) (SDL.RendererConfig { SDL.rendererType = SDL.UnacceleratedRenderer, SDL.rendererTargetTexture = False })
+            --rendererInfo <- SDL.getRendererInfo renderer
+            --putStrLn $ show rendererInfo
+            appLoop renderer
+        catchErr window exception = do
+            SDL.destroyWindow window
+            putStrLn "Caught exception!"
+            putStrLn $ show (exception :: Exception.ErrorCall)
+    
+
+appLoop renderer = do
+    events <- SDL.pollEvents
+    let qPressed = any eventIsPressQ events
+    unless qPressed (appLoop renderer)
+
+
