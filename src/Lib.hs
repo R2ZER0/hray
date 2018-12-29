80ce140ecfe60e0d4fec96356c8102f3d36bd1d1
Rikki Guy <git@rikkiguy.me.uk>
2018-12-29 13:30:14 +0000

    Its all or nothing!

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,24 +10,30 @@ import qualified SDL as SDL
 import qualified Control.Exception as Exception
 import Foreign.C.Types (CInt)
 
-width = 200
-height = 100
+width = 400
+height = width `div` 2
 
+-- Colours
 white = CVec3 1.0 1.0 1.0
+red = CVec3 1.0 0.0 0.0
+sunsetRed = (CVec3 255 166 158) .^ (1.0 / 255.0)
 skyBlue = (CVec3 135 206 235) .^ (1.0 / 255.0)
 
 vec x y z = CVec3 x y z
 
--- Ray origin direction
+-- Ray point direction
 data Ray = Ray CVec3 CVec3 deriving (Show, Eq)
+
+-- Sphere point radius
+data Sphere = Sphere CVec3 Double
 
 lowerLeft = vec (-0.5) (-0.5) 1.0
 xUnit xs = vec (1.0/(fromIntegral xs)) 0 0
 yUnit ys = vec 0 (1.0/(fromIntegral ys)) 0
 
-cameraLocation = vec 0 0 1
+cameraLocation = vec 0 0 2
 
-rayAt xs ys x y = Ray cameraLocation (lowerLeft <+> ((xUnit xs) .^ (fromIntegral x)) <+> ((yUnit ys) .^ (fromIntegral y)))
+rayAt xs ys x y = Ray cameraLocation (normalize (lowerLeft <+> ((xUnit xs) .^ (fromIntegral x)) <+> ((yUnit ys) .^ (fromIntegral y))))
 
 viewPixelsAt xs ys x y
     | x >= xs && y >= ys = []
@@ -35,11 +41,7 @@ viewPixelsAt xs ys x y
     | otherwise = (x, y) : viewPixelsAt xs ys (x+1) y
 
 viewPixels xs ys = viewPixelsAt xs ys 0 0
-
 viewRays xs ys = fmap (\(x, y) -> (x, y, rayAt xs ys x y)) $ viewPixels xs ys
-
-trace (Ray origin dir@(CVec3 xd yd zd)) = gradient white skyBlue yd
-
 view xs ys = fmap (\(x, y, ray) -> (x, y, trace ray)) $ viewRays xs ys
 
 someFunc :: IO ()
@@ -52,6 +54,25 @@ someFunc = do
 gradient from to t = (from .^ t') <+> (to .^ (1-t'))
     where
         t' = max 0.0 (min 1.0 t)
+
+intersectSphere :: Sphere -> Ray -> Maybe (Double, CVec3)
+intersectSphere (Sphere sphereCentre sphereRadius) (Ray rayOrigin rayDirection) =
+    if discriminant < 0 then Nothing else Just (t, rayOrigin <+> (rayDirection .^ t))
+    where
+        cameraToCentre = rayOrigin <-> sphereCentre
+        v = cameraToCentre .* rayOrigin
+        eoDot = cameraToCentre .* cameraToCentre
+        discriminant = (sphereRadius * sphereRadius) - eoDot + (v * v)
+        t = v - (sqrt discriminant)
+
+-- The Scene
+sphere1 = Sphere (CVec3 0.0 0.0 (-2.5)) 0.05
+
+-- Trace
+trace ray@(Ray origin dir@(CVec3 xd yd zd)) = case intersectSphere sphere1 ray of
+    Just (t, point) -> red
+    Nothing -> gradient sunsetRed skyBlue yd
+
 
 -- Utility functions
 eventIsPressQ ev = case SDL.eventPayload ev of
@@ -72,7 +93,7 @@ renderPicture img = do
             SDL.destroyWindow window
             putStrLn "Caught exception!"
             putStrLn $ show (exception :: Exception.ErrorCall)
-    
+
 
 drawPoint renderer (x, y, CVec3 r g b) = do
     let drawColour = SDL.rendererDrawColor renderer
